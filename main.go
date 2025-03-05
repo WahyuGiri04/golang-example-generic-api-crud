839efe7ad42069470540a06e670cdd108fa388f8
Giri <88410452+WahyuGiri04@users.noreply.github.com>
2025-03-05 09:08:36 +0700

    update route

--- main.go
+++ main.go
@@ -28,11 +28,6 @@ func main() {
 	// Setup routes
 	routes.SetupRoutes(r)
 
-	// Endpoint health check
-	r.GET("/health", func(c *gin.Context) {
-		c.JSON(200, gin.H{"status": "OK"})
-	})
-
 	// Jalankan server
 	log.Printf("Server berjalan di port %s...", port)
 	r.Run(":" + port)
--- routes/routes.go
+++ routes/routes.go
@@ -12,6 +12,11 @@ func SetupRoutes(route *gin.Engine) {
 	var path = os.Getenv("CONTEXT_PATH")
 
 	RoleController := controller.NewRoleController()
+	// Endpoint health check
+	route.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{"status": "UP"})
+	})
+
 	roleRoutes := route.Group(path + "/role")
 	roleRoutes.Use(middleware.AuthMiddleware())
 	{
